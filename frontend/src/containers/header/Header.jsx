6e6470cd0b41a684a96155bb1b53eeaab3ad37e0
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-02-20 02:31:05 +0100

    header completed

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something Amazing with ChatGPT3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eos
+          alias nesciunt ex aspernatur, molestiae obcaecati minus, voluptatum
+          cum sint dignissimos. Voluptatibus nemo voluptate delectus perferendis
+          quidem blanditiis accusantium dolore!
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people profiles" />
+          <p>1600 people joined our platform last month </p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
     </div>
   )
 }
