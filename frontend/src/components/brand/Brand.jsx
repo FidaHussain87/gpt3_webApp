18518c3d0ea477dd58493a19da98406317916d45
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-02-22 03:09:56 +0100

    heading, whatgpt3,features component added

--- frontend/src/components/brand/Brand.jsx
+++ frontend/src/components/brand/Brand.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './imports'
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} atl="google" />
+      </div>
+      <div>
+        <img src={slack} atl="slack" />
+      </div>
+      <div>
+        <img src={atlassian} atl="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} atl="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} atl="shopify" />
+      </div>
     </div>
   )
 }
--- frontend/src/containers/whatGPT3/WhatGPT3.jsx
+++ frontend/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components'
+
 const WhatGPT3 = () => {
   return (
-    <div>
-      What GPT3
+    <div className="gpt3__whatgpt3 section__margin" id="whatgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT3"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ducimus quo architecto dicta! Possimus voluptatum, alias eaque quo veritatis deleniti esse doloribus, eos delectus soluta, architecto blanditiis dignissimos quasi modi?
+"
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The Possibilities are beyond your Imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ducimus quo architecto dicta! Possimus voluptatum, alias eaque quo veritatis deleniti esse doloribus, eos delectus soluta, architecto blanditiis dignissimos quasi modi?
+"
+        />
+        <Feature
+          title="Knowledgebase"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ducimus quo architecto dicta! Possimus voluptatum, alias eaque quo veritatis deleniti esse doloribus, eos delectus soluta, architecto blanditiis dignissimos quasi modi?
+"
+        />
+        <Feature
+          title="Education"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ducimus quo architecto dicta! Possimus voluptatum, alias eaque quo veritatis deleniti esse doloribus, eos delectus soluta, architecto blanditiis dignissimos quasi modi?
+"
+        />
+      </div>
     </div>
   )
 }
-
 export default WhatGPT3
