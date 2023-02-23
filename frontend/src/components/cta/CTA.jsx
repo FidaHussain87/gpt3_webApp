a97147e4ef7109a24682caeb6043bd7da4a5a0c3
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-02-23 02:30:09 +0100

    CTA completed

--- frontend/src/components/cta/CTA.jsx
+++ frontend/src/components/cta/CTA.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './cta.css'
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today and start exploring the endless possibilities.</h3>
+      </div>
+      <div className='gpt3__cta-button'><button type="button">Get Started</button></div>
     </div>
   )
 }
--- frontend/src/containers/possibility/Possibility.jsx
+++ frontend/src/containers/possibility/Possibility.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 const Possibility = () => {
   return (
-    <div>
-      Possibility
+    <div className="gpt3__possibility section__padding " id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="Possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The Possibilities are beyond your imagination
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus fugit
+          aut assumenda blanditiis dolorum saepe ad possimus repellendus.
+          Tenetur repudiandae ab quos voluptate! Quod laborum neque voluptatem
+          eius numquam enim?
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   )
 }
